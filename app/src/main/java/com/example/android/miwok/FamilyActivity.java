fc9ddc3ac3d608cef0a326349c5fb183a3191c7b
Solariii <eslamsolari1996@gmail.com>
2020-04-13 01:59:23 +0200

    added MediaPlayer and AudioManager

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,33 +2,102 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
+
+    private AudioManager audioManager;
+
+    AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN){
+                mediaPlayer.start();
+            }else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                mediaPlayer.release();
+            }
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener mCompleteListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-        ArrayList<Word> family = new ArrayList<Word>();
-        family.add(new Word("father", "әpә", R.drawable.family_father));
-        family.add(new Word("mother", "әṭa",R.drawable.family_mother));
-        family.add(new Word("son", "angsi", R.drawable.family_son));
-        family.add(new Word("daughter", "tune", R.drawable.family_daughter));
-        family.add(new Word("older brother", "taachi", R.drawable.family_older_brother));
-        family.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother));
-        family.add(new Word("older sister","teṭe", R.drawable.family_older_sister));
-        family.add(new Word("younger sister","kolliti", R.drawable.family_younger_sister));
-        family.add(new Word("grandmother","ama", R.drawable.family_grandmother));
-        family.add(new Word("grandfather","paapa", R.drawable.family_grandfather));
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        final ArrayList<Word> family = new ArrayList<Word>();
+        family.add(new Word("father", "әpә", R.drawable.family_father, R.raw.family_father));
+        family.add(new Word("mother", "әṭa",R.drawable.family_mother, R.raw.family_mother));
+        family.add(new Word("son", "angsi", R.drawable.family_son, R.raw.family_son));
+        family.add(new Word("daughter", "tune", R.drawable.family_daughter, R.raw.family_daughter));
+        family.add(new Word("older brother", "taachi", R.drawable.family_older_brother, R.raw.family_older_brother));
+        family.add(new Word("younger brother", "chalitti", R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        family.add(new Word("older sister","teṭe", R.drawable.family_older_sister, R.raw.family_older_sister));
+        family.add(new Word("younger sister","kolliti", R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        family.add(new Word("grandmother","ama", R.drawable.family_grandmother, R.raw.family_grandmother));
+        family.add(new Word("grandfather","paapa", R.drawable.family_grandfather, R.raw.family_grandfather));
 
         WordAdapter itemsAdapter = new WordAdapter(this, family, R.color.category_family);
 
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Word word = family.get(position);
+                releaseMediaPlayer();
+                int result = audioManager.requestAudioFocus(onAudioFocusChangeListener,AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mediaPlayer = MediaPlayer.create(FamilyActivity.this, word.getmAudioResourceId());
+                    mediaPlayer.start();
+                    mediaPlayer.setOnCompletionListener(mCompleteListener);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer () {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+            audioManager.abandonAudioFocus(onAudioFocusChangeListener);
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,19 +5,22 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int imageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId;
     private static final int NO_IMAGE_PROVIDED = -1;
 
 
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation, int imageResourceId) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int imageResourceId, int mAudioResourceId) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
         this.imageResourceId = imageResourceId;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
-    public Word(String mDefaultTranslation, String mMiwokTranslation) {
+    public Word(String mDefaultTranslation, String mMiwokTranslation, int mAudioResourceId) {
         this.mDefaultTranslation = mDefaultTranslation;
         this.mMiwokTranslation = mMiwokTranslation;
+        this.mAudioResourceId = mAudioResourceId;
     }
 
     public String getmDefaultTranslation() {
@@ -34,5 +37,19 @@ public class Word {
 
     public boolean hasImage() {
         return  imageResourceId != NO_IMAGE_PROVIDED ;
+    }
+
+    public int getmAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", imageResourceId=" + imageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
     }
 }

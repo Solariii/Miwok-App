3c3653fd67b60a363665c4100522c30246569413
Solariii <eslamsolari1996@gmail.com>
2020-04-14 21:07:59 +0200

    Added tabs

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,12 +1,18 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
-    public CategoryAdapter(FragmentManager fm){
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm){
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -25,6 +31,19 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
 
